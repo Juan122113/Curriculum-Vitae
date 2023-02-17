ccfb1267bbc2557531a8376cb26f0d381f81dab6
Juan122113 <ammiel89@hotmail.com>
2023-02-17 16:30:34 -0300

    Code changes

--- scripts.js
+++ scripts.js
@@ -1,5 +1,23 @@
-document.getElementById("hamburguer").addEventListener('click', function() {
-
-  document.getElementById("nav").style.display = 'contents';
+document.getElementById("hamburguer").onclick = function () {
   
-  });
+  document.getElementById("nav").style.display = "contents";
+  
+  }
+
+document.getElementById("content").onclick = function () {
+  
+    document.getElementById("nav").style.display = "none";
+    
+    }
+
+const collection = document.getElementsByClassName("subtitle");
+
+    for (let  i = 0; i < collection.length; i++) {
+    
+    collection[i].style.backgroundColor = "aliceblue";
+    
+    }
+ 
+
+  
+  
